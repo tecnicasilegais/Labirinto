792d0c7bd89bb6f65cc5d5beec4c745ea6479d27
Eduardo C. Andrade <eduardo.a@edu.pucrs.br>
2021-09-16 03:35:09 -0300

    Maze rendering done

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,8 @@
                                                v-if="loading"></v-skeleton-loader>
                             <v-card-text v-else>
                                 <label for="file_maze" class="pl-0">Arquivo de labirinto</label>
-                                <v-file-input class="mb-3" dense outlined hide-details="auto" v-model="mazeFile"
-                                              @click="fileError = []" 
+                                <v-file-input class="mb-3" dense outlined hide-details="auto" v-model="maze.file"
+                                              @click="fileError = []"
                                               :error-messages="fileError" small-chips
                                               id="file_maze"></v-file-input>
 
@@ -61,12 +61,28 @@ isso nao pode ser</pre>
                             <v-skeleton-loader type="table" v-if="loading"></v-skeleton-loader>
                             <v-card-text v-else>
                                 <v-row>
-                                    <v-col v-if="mazeContent">
-                                        <v-row v-for="(line, i) in mazeParsed" :key="i">
-                                            <v-col v-for="(cell, j) in line" :key="j">
-                                                {{ cell }}
-                                            </v-col>
-                                        </v-row>
+                                    <v-col v-if="maze.rawContent" class="d-flex justify-center">
+                                        <table class="maze">
+                                            <tr>
+                                                <td :style="maze.background.tl"></td>
+                                                <td :style="maze.background.t" v-for="i in maze.size" :key="i"></td>
+                                                <td :style="maze.background.tr"></td>
+                                            </tr>
+                                            <tr v-for="(line, i) in maze.parsedContent" :key="i">
+                                                <td :style="maze.background.l"></td>
+                                                <td v-for="(cell, j) in line" :key="j" :style="maze.background.grass">
+                                                    <div v-if="cell === 'wall'" :style="maze.walls[(i+j)%4]"></div>
+                                                    <div v-else-if="cell === 'entr'" :style="maze.doors.entrance"></div>
+                                                    <div v-else-if="cell === 'exit'" :style="maze.doors.exit"></div>
+                                                </td>
+                                                <td :style="maze.background.r"></td>
+                                            </tr>
+                                            <tr>
+                                                <td :style="maze.background.bl"></td>
+                                                <td :style="maze.background.b" v-for="i in maze.size" :key="i"></td>
+                                                <td :style="maze.background.br"></td>
+                                            </tr>
+                                        </table>
                                     </v-col>
                                     <v-col v-else class="text-center subtitle-1 font-weight-bold">
                                         Informe seu arquivo!
@@ -87,32 +103,81 @@ import 'vue-code-highlight/themes/window.css';
 
 export default {
     data   : () => ({
-        loading    : false,
-        mazeFile   : null,
-        mazeContent: null,
-        mazeParsed : null,
-        fileError  : []
+        loading  : false,
+        maze     : {
+            file         : null,
+            rawContent   : null,
+            parsedContent: null,
+            size         : null,
+            background   : {
+                grass: {backgroundImage: `url(${require('@/assets/grass.png')})`},
+                tl   : {backgroundImage: `url(${require('@/assets/tl.png')})`},
+                t    : {backgroundImage: `url(${require('@/assets/t.png')})`},
+                tr   : {backgroundImage: `url(${require('@/assets/tr.png')})`},
+                l    : {backgroundImage: `url(${require('@/assets/l.png')})`},
+                r    : {backgroundImage: `url(${require('@/assets/r.png')})`},
+                bl   : {backgroundImage: `url(${require('@/assets/bl.png')})`},
+                b    : {backgroundImage: `url(${require('@/assets/b.png')})`},
+                br   : {backgroundImage: `url(${require('@/assets/br.png')})`}
+            },
+            walls        : [
+                {backgroundImage: `url(${require('@/assets/w1.png')})`},
+                {backgroundImage: `url(${require('@/assets/w2.png')})`},
+                {backgroundImage: `url(${require('@/assets/w3.png')})`},
+                {backgroundImage: `url(${require('@/assets/w4.png')})`}
+            ],
+            doors        : {
+                entrance: {backgroundImage: `url(${require('@/assets/entrance.png')})`},
+                exit    : {backgroundImage: `url(${require('@/assets/exit.png')})`}
+            }
+        },
+        fileError: []
     }),
     methods: {
         importTxt() {
-            if (!this.mazeFile) {
-                this.mazeContent = null;
-                this.fileError   = ['Informe um arquivo!'];
+            if (!this.maze.file) {
+                this.maze.rawContent = null;
+                this.fileError       = ['Informe um arquivo!'];
                 return;
             }
             let reader = new FileReader();
 
-            reader.readAsText(this.mazeFile);
+            reader.readAsText(this.maze.file);
             reader.onload  = () => {
-                this.mazeContent = reader.result;
-                this.mazeParsed  = this.mazeContent.split(/\n|\r\n/);
-                if (this.mazeParsed.length > 18) {
+                this.maze.rawContent = reader.result;
+                let rawLines         = this.maze.rawContent.split(/\n|\r\n/);
+                this.maze.size       = parseInt(rawLines.shift());
+                if (this.maze.size <= 1) {
+                    this.fileError = ['Labirinto muito pequeno!'];
+                } else if (this.maze.size > 18) {
                     this.fileError = ['Labirinto muito grande!'];
                 }
-                for (let i = 0; i < this.mazeParsed.length; i++) {
-                    this.mazeParsed[i] = this.mazeParsed[i].split(' ');
+
+                this.maze.parsedContent = [];
+                for (let i = 1; i <= this.maze.size; i++) {
+                    let convertedLine = i === 0 || i === this.maze.size + 1 ?
+                                        Array.from({length: this.maze.size + 2}) :
+                                        rawLines[i - 1].split(' ');
+                    for (let j = 0; j < this.maze.size; j++) {
+                        let cell = convertedLine[j];
+                        switch (cell) {
+                            case 'E':
+                                convertedLine[j] = 'entr';
+                                break;
+                            case 'S':
+                                convertedLine[j] = 'exit';
+                                break;
+                            case '0':
+                                convertedLine[j] = '';
+                                break;
+                            case '1':
+                                convertedLine[j] = 'wall';
+                                break;
+                        }
+                    }
+                    this.maze.parsedContent.push(convertedLine);
                 }
-                if (this.mazeParsed.length > 18) {
+                if (this.maze.parsedContent.length > 18) {
                     this.fileError = ['Labirinto muito grande!'];
                 }
             };
@@ -124,4 +189,19 @@ export default {
 </script>
 
 <style lang="scss">
+table.maze {
+    border          : none;
+    border-collapse : collapse;
+
+    > tr > td {
+        height     : 48px;
+        width      : 48px;
+        text-align : center;
+
+        > div {
+            height : 100%;
+            width  : 100%;
+        }
+    }
+}
 </style>
