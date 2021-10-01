f5a385ffd0632312fb667977b962021ad6d7866b
Augusto <guto9bottega@gmail.com>
2021-10-01 00:55:39 -0300

    Start of the cycle and creation of the neighbor

--- src/app.js
+++ src/app.js
@@ -39,6 +39,77 @@ function nOfMovementsToExit(currPos) {
   return Math.abs(currPos.line - _exit.line) + Math.abs(currPos.col - _exit.col);
 }
 
+function buildNextPath(path) {
+
+  let hit = false;
+  const currPosition = { line: _entrance.line, col: _entrance.col };
+  const positions = [];
+  for (const movement of path) {
+    _maze[currPosition.line][currPosition.col].wasVisited = true;
+    positions.push(_maze[currPosition.line][currPosition.col]);
+
+    switch (movement) {
+      case 'U':
+        currPosition.line--;
+        break;
+      case 'D':
+        currPosition.line++;
+        break;
+      case 'R':
+        currPosition.col++;
+        break;
+      case 'L':
+        currPosition.col--;
+        break;
+      default:
+        throw new Error('Invalid movement');
+    }
+    if (outOfMaze(currPosition)) {
+      hit = true;
+    } else {
+      if (hitsWall(_maze[currPosition.line][currPosition.col])) {
+        hit = true;
+      }
+      if (alreadyVisited(_maze[currPosition.line][currPosition.col])) {
+        hit = true;
+      }
+    }
+  }
+
+  //TODO: essas 2 vars precisam vir do front
+  prctFixed = 0.6; //porcentagem de vezes que ele resolve o primeiro errado 
+  prctGoodChoice = 0.6
+
+  firstWrong //primeiro nodo incorreto no path
+
+  let fixOptions = []//ao percorrer o path, posso pegar as possibilidades junto e as posições delas pra talvez modificar elas
+
+  //TODO: ao criar o _maze podemos adicionar um atributo que mostre as possibilidades de movimento do nodo
+
+  if(!hit) {
+      let options = 'CDBE';
+      path += options[Math.random()|0];
+  } else {
+    porcentage = Math.random();
+    if (prctFixed >= porcentage) {
+        //Caso tenha que arrumar o primeiro errado
+          //tenta modificar para uma das possibilidades de movimento
+          //ou caso nao tenha possibilidades, pega aleatoriamente umas das outras 3 opções de movimento
+    }else {
+      //caso que pega uma posição aleatoria da sequencia e muda conforme a porcentagem de escolhas boas
+      let pos = Math.random() * fixOptions.size();
+      if(prctGoodChoice <= Math.random()){
+        //caso que pega a posição escolhida e bota um movimento aleatoria diferente do atual
+        move = options[Math.random() * options, 10|0];
+        //MODIFICAR AQUI, TA ERRADO
+        path[pos] = options[Math.random() * options, 10|0];
+      }else {
+        //caso que pega a posição escolhida e bota um movimento válido
+      }
+    }
+  }
+}
+
 function calculateFitness(path) {
   let fitness = 0;
 
@@ -99,11 +170,23 @@ export function findPath(maze, { entrance, exit }, parameters) {
 
   _output += `ciclos: ${parameters.cycles}, tempInicial: ${parameters.tempInitial}, variaçãoTemp: ${parameters.tempVariation}\n`;
 
-  const initialPath = generateString(1);
-  const workingPath = calculateFitness(initialPath);
+  const currentPath = generateString(1);
+  const nextPath;
+  const workingPath = calculateFitness(currentPath);
 
   const randomNumber = rn();
   _output += `${randomNumber}\n`;
+
+
+  //Start the cycle until numInteractions is reached
+  for (let i = 0; i <= parameters.cycles; i++) {
+    
+    //get heuristic of currently path
+    // if h(currently) == 0 -> end of algorithm
+    nextPath = buildNextPath(currentPath)
+
+
+  }
 
   //TODO: ciclos e temperatura
   //Conforme temperatura abaixa, muda menos os movimentos iniciais
