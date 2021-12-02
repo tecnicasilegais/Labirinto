191b8e4ee4670bdfe1347d837b4698b0006078d4
Alessandro Medeiros <alessandro_m_13@hotmail.com>
2021-12-01 23:36:50 -0300

    Redes e Neuronio

---
+++ src/Rede/Neuronio.js
@@ -0,0 +1,54 @@
+export default class Neuronio {
+
+    pesos=null;
+    defineFuncao;
+
+    constructor(pesos) {
+        this.setPesos(pesos);
+        this.defineFuncao=0;
+    }
+
+    setPesos(pesos){
+        this.pesos = pesos;
+    }
+
+    setFuncao(defineFuncao){
+        this.defineFuncao = defineFuncao;
+    }
+
+    calculaY(x) {
+        var v = 0;
+        var i;
+
+        for (i = 0; i < x.length; i++) {
+            v = v + this.pesos[i] * x[i];
+        }
+        v = v + this.pesos[i];
+
+        switch (this.defineFuncao) {
+            case 1:
+                this.tangenteHiperbolica(v);
+                break;
+            default:
+                this.logistica(v);
+        }
+    }
+
+    logistica(v) {
+        return 1 / (1 + Math.exp(-v));
+    }
+
+
+    tangenteHiperbolica(v) {
+        return Math.tanh(v);
+    }
+
+    toString() {
+        var msg = "";
+        var i;
+        for (i = 0; i < this.pesos.length; i++) {
+            msg = msg + this.pesos[i] + " ";
+        }
+        return msg;
+    }
+}
---
+++ src/Rede/Rede.js
@@ -0,0 +1,67 @@
+import Neuronio from "./Neuronio";
+
+export default class Rede {
+
+    camadaOculta = [];
+    camadaSaida = [];
+    saida = [];
+
+    constructor(numNeuroniosOculta, numNeuroniosSaida) {
+        if (numNeuroniosOculta <= 0 || numNeuroniosSaida <= 0) {
+            numNeuroniosOculta = 8;
+            numNeuroniosSaida = 4;
+        }
+        this.camadaOculta = new Neuronio(numNeuroniosOculta);
+        this.camadaSaida = new Neuronio(numNeuroniosSaida);
+    }
+
+    setPesosNaRede(numEntradas, pesos) {
+        var k = 0;
+        var tmp = [];
+
+        for (var i = 0; i < this.camadaOculta.length; i++) {
+            tmp = (numEntradas + 1);
+            for (var j = 0; j < numEntradas + 1; j++) {
+                tmp[j] = pesos[k];
+                k++;
+            }
+            this.camadaOculta[i]=new Neuronio(tmp);
+        }
+
+        for (var i = 0; i < this.camadaSaida.length; i++) {
+            tmp = (this.camadaOculta.length+1);
+            for (var j = 0; j < this.camadaOculta.length + 1; j++) {
+                tmp[j] = pesos[k];
+                k++;
+            }
+            this.camadaSaida[i] = new Neuronio(tmp); //chamando classe Neuronio
+        }
+    }
+
+    propagacao(x) {
+        if (x == null) return null;
+
+        this.saidaOculta = this.camadaOculta.length;
+        this.saida = this.camadaSaida.length;
+        for (var i = 0; i < this.camadaOculta.length; i++) {
+            this.saidaOculta[i] = this.camadaOculta[i].calculaY(x);
+        }
+        for (var i = 0; i < this.camadaSaida.length; i++) {
+            this.saida[i] = this.camadaSaida[i].calculaY(this.saidaOculta);
+        }
+        return this.saida;
+    }
+
+    toString() {
+        var msg = "Pesos da rede\n";
+        msg = msg + "Camada Oculta\n";
+        for (var i = 0; i < this.camadaOculta.length; i++) {
+            msg = msg + "Neuronio " + i + ": " + this.camadaOculta[i] + "\n";
+        }
+        msg = msg + "Camada Saida\n";
+        for (var i = 0; i < this.camadaSaida.length; i++) {
+            msg = msg + "Neuronio " + i + ": " + this.camadaSaida[i] + "\n";
+        }
+        return msg;
+    }
+}
